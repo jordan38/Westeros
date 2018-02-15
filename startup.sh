2a84a8348698d90dfa60c4a9907d4bf467dd94b9
Jordan <jordan.content@outlook.fr>
2018-02-15 21:59:35 +0100

    * Modification du startup et ajout du téléchargement de fichier conf ddclient * Ajout du fichier de conf nginx

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 UTILISATEUR_COURANT=$(whoami)
+DDCLIENT_CHEMIN=/opt/dockers/ddclient/config
+COMPOSE_FICHIER_CHEMIN=~/devs
 
 sudo apt-get update
 
@@ -29,12 +31,20 @@ sudo apt-get install docker-ce
 ## Ajoute l'utilisateur au groupe docker
 sudo usermod -a -G docker $UTILISATEUR_COURANT
 
-## mise en place de docker-compose
+## Mise en place de docker-compose
 echo "Installation Docker compose"
 sudo curl -L --fail https://github.com/docker/compose/releases/download/1.19.0/run.sh -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-## Recuperer le docker-compose.yml
+## Récuperer le fichier de conf de ddclient
+echo "Mise en place des fichiers de configuration"
 
-wget https://raw.githubusercontent.com/jordan38/Westeros/master/docker-compose.yml -o /tmp/docker-compose.yml
+sudo mkdir -p $DDCLIENT_CHEMIN
+wget https://raw.githubusercontent.com/jordan38/Westeros/master/ddclient/ddclient.conf $DDCLIENT_CHEMIN
+
+## Recuperer le docker-compose.yml
+echo "Mise en place du docker compose file"
+
+sudo mkdir -p $COMPOSE_FICHIER_CHEMIN
+wget https://github.com/jordan38/Westeros/blob/master/docker/docker-compose.yml $COMPOSE_FICHIER_CHEMIN
 

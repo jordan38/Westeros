df6aa1e61b9fef58e7a49d0cb16a40ba776c5cea
Jordan <jordan.content@outlook.fr>
2018-03-13 22:40:24 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -49,5 +49,3 @@ echo "Mise en place du docker compose file"
 
 mkdir -p $COMPOSE_FICHIER_CHEMIN
 wget https://raw.githubusercontent.com/jordan38/Westeros/master/docker/docker-compose.yml -P $COMPOSE_FICHIER_CHEMIN
-
-reboot now

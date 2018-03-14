ff5b615cbd0c927c8145dac671fe90b3c6e00338
Jordan <jordan.content@outlook.fr>
2018-03-15 00:30:58 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+CHEMIN_DOSSIER_SCRIPT=~/devs
 CHEMIN_FICHIER_COMPOSE=~/devs/docker-compose.yml
 
 if [ -f $CHEMIN_FICHIER_COMPOSE ]; then
     rm -f $file
 fi
 
-wget https://raw.githubusercontent.com/jordan38/Westeros/master/docker/docker-compose.yml -P $CHEMIN_FICHIER_COMPOSE
+wget https://raw.githubusercontent.com/jordan38/Westeros/master/docker/docker-compose.yml -P $CHEMIN_DOSSIER_SCRIPT

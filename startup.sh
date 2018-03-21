9f3b308d32e36f05bb2eeda5410d242d96c6a1fb
Jordan <jordan.content@outlook.fr>
2018-03-21 13:20:21 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -53,14 +53,14 @@ wget https://raw.githubusercontent.com/jordan38/Westeros/master/ddclient/ddclien
 echo "Mise en place des fichiers nginx"
 
 NGINX_CONF_CHEMIN=/var/nginx
-NGINX_HTML_CHEMIN=/var/nginx/www/html
+NGINX_HTML_CHEMIN=/var/nginx/html
 NGINX_SITES_AVAILABLE_CHEMIN=/var/nginx/sites-available
 NGINX_SITES_ENABLED_CHEMIN=/var/nginx/conf.d
 
 # Les fichiers de configurations globaux
 mkdir -p $NGINX_CONF_CHEMIN
 wget https://raw.githubusercontent.com/jordan38/Westeros/master/nginx/conf/nginx.conf -P $NGINX_CONF_CHEMIN
-#wget https://raw.githubusercontent.com/jordan38/Westeros/master/nginx/conf/mime.types -P $NGINX_CONF_CHEMIN
+wget https://raw.githubusercontent.com/jordan38/Westeros/master/nginx/conf/mime.types -P $NGINX_CONF_CHEMIN
 # Les fichiers de configuration des servers block
 mkdir -p $NGINX_SITES_AVAILABLE_CHEMIN
 wget https://raw.githubusercontent.com/jordan38/Westeros/master/nginx/sites-available/nextcloud.eastwatch.me -P $NGINX_SITES_AVAILABLE_CHEMIN
@@ -68,7 +68,7 @@ wget https://raw.githubusercontent.com/jordan38/Westeros/master/nginx/sites-avai
 # Les fichiers de configuration des servers block activés
 mkdir -p $NGINX_SITES_ENABLED_CHEMIN
 #cp /var/nginx/sites-available/nextcloud.eastwatch.me /var/nginx/conf.d/nextcloud.eastwatch.me.conf
-#cp /var/nginx/sites-available/portainer.eastwatch.me /var/nginx/conf.d/portainer.eastwatch.me.conf
+ln -s /var/nginx/sites-available/portainer.eastwatch.me /var/nginx/conf.d/portainer.eastwatch.me.conf
 
 ## Recuperer le docker-compose.yml
 echo "Mise en place du docker compose file"
